d80c74e9a96049e38daf6e79e5f3a7b5ad9afcdc
abbey-glitch <abiodunonaolapi@gmail.com>
2023-11-10 13:17:08 +0100

    incrementing value using setstate

--- src/component/count.jsx
+++ src/component/count.jsx
@@ -5,11 +5,11 @@ class Number extends Component {
         {id:1, value:3},
         {id:2, value:0},
         {id:3, value:5},
-        {id:4, value:6}
+        {id:4, value:8}
     ]} 
-    handleIncrement = () =>{
-        console.log(this.state.nums)
-    }
+    // handleIncrement = () =>{
+    //     console.log(this.state.nums)
+    // }
     render() { 
         // console.log(this.state.nums)
         return (<>
@@ -20,6 +20,10 @@ class Number extends Component {
                     <Numbers 
                     key={num.id}
                     num={num}>
+                        {/* passing of props to a component */}
+                      <p>this is the child props</p>
+                      <h3>welcome</h3>
+                      <button>Delete</button>
                     </Numbers>)
                 }
                 </div>
--- src/component/counts.jsx
+++ src/component/counts.jsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 class Numbers extends Component {
-    state = { nums:0 } 
+    state = { nums:this.props.num.value } 
     handleIncrement = ()=>{
         this.setState({num:this.state.nums++})
     }
     render() { 
-        console.log(this.props.num.value);
+        console.log(this.props.children[2].props.children);
         return (<>
               
                <div className='bg-info mx-4'>
                    <span className='p-2 border border-warning mx-2 bg-light'>{this.state.nums}</span>
                    <button className='bg-success' onClick={this.handleIncrement}>Increment</button>
                    <button className='bg-danger'>Decrement</button>
+                   <button>{this.props.children[2].props.children}</button>
                </div>
         </>);
     }
